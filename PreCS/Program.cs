38931c9c62c6d4b2e1c3bfd76b4e23faa96bf058
Riey <tkfkzo@daum.net>
2016-12-20 22:06:32 +0900

    Improved readability

--- PreCS/Program.cs
+++ PreCS/Program.cs
@@ -16,9 +16,10 @@ namespace PreCS
         {
             var refAsm = Assembly.Load(File.ReadAllBytes(args[0]));
             var refModule = refAsm.ManifestModule;
+
             var asm = AssemblyDefinition.ReadAssembly(args[0]);
             var module = asm.MainModule;
-            var builderAttribute = module.Import(typeof(BuilderAttribute));
+
             var allTypes = module.GetTypes().Skip(1).ToArray();//<Module> Skip
             var allRefTypes = refModule.GetTypes();
             var allMethods = (
@@ -28,6 +29,7 @@ namespace PreCS
                 where !method.IsConstructor
                 select (fullName, method))
                 .ToDictionary(t => t.Item1, t => t.Item2);
+
             var allRefMethods = (
                 from type in allRefTypes
                 from method in type.GetRuntimeMethods()
@@ -35,16 +37,21 @@ namespace PreCS
                 where allMethods.ContainsKey(fullName)
                 select (fullName, method))
                 .ToDictionary(t => t.Item1, t => t.Item2);
+
             var builderMethods = allRefMethods.Where(m => m.Value.IsDefined(typeof(BuilderAttribute)))
                 .ToDictionary(p => p.Key, p => p.Value);//Cache BuilderMethods
+
             var initializerMethods = allRefMethods.Where(m => m.Value.IsDefined(typeof(InitializerAttribute))).ToArray()
                 .ToDictionary(p => p.Value.GetCustomAttribute<InitializerAttribute>().TargetName, p => p.Value);//Cache InitializerMethods
+
             foreach (var method in allMethods)
             {
                 var body = method.Value.Body;
                 var il = body.GetILProcessor();
-                List<(Instruction oldInst, Instruction newInst)> replaceList = new List<(Instruction, Instruction)>();
-                List<Instruction> removeList = new List<Instruction>();
+
+                var replaceList = new List<(Instruction oldInst, Instruction newInst)>();
+                var removeList = new List<Instruction>();
+
                 foreach (var instruction in body.Instructions)
                 {
                     switch (instruction.OpCode.Code)
@@ -54,17 +61,21 @@ namespace PreCS
                         case Code.Callvirt:
                             {
                                 var calledMethod = (instruction.Operand as MethodReference).Resolve();
+
                                 if (!allRefMethods.ContainsKey(GetFullName(calledMethod)))//Defined by Extern Library
                                     break;
+
                                 var calledRefMethod = allRefMethods[GetFullName(calledMethod)];
                                 if (builderMethods.ContainsValue(calledRefMethod))
                                 {
                                     MethodInfo builder = builderMethods[GetFullName(calledMethod)];
                                     MethodInfo initializer = initializerMethods[builder.GetCustomAttribute<BuilderAttribute>().Name];
+
                                     List<Instruction> addTemp = new List<Instruction>();
                                     Stack<object> callStack = new Stack<object>();
                                     int parameterLength = calledMethod.Parameters.Count;
                                     Instruction temp = instruction.Previous;
+
                                     try
                                     {
                                         for (int i = 0; i < parameterLength; i++)
@@ -78,23 +89,31 @@ namespace PreCS
                                     {
                                         break;
                                     }
+
                                     removeList.AddRange(addTemp);//Remove parameter opcodes
+
                                     initializer.Invoke(null, null);//Invoke Initializer
+
                                     var result = builder.Invoke(null, callStack.ToArray());//Invoke Builder
+
                                     replaceList.Add((instruction, il.Save(result)));//Replace Call OpCode to result
+
                                     Helper.ClearField();
                                 }
                                 break;
                             }
                     }
                 }
+
                 foreach (var replace in replaceList)
                     il.Replace(replace.oldInst, replace.newInst);
                 foreach (var remove in removeList)
                     il.Remove(remove);
             }
+
             asm.Write(args[0]);//Save modified assembly
         }
+
         private static string GetFullName(MethodDefinition method) => method.DeclaringType.FullName + "." + method.Name;
         private static string GetFullName(MethodInfo method) => method.DeclaringType.FullName + "." + method.Name;
     }
